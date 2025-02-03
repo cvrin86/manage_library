dd400a8abfd75bad6273f9dd4fda54f631ac38d1
CristinaV Doranco <cristinavranceanu86@gmail.com>
2025-02-03 10:58:56 +0100

    Connexion to database MySql

--- src/main/java/fr/doranco/biblio/BiblioApplication.java
+++ src/main/java/fr/doranco/biblio/BiblioApplication.java
@@ -3,7 +3,8 @@ package fr.doranco.biblio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication // @ veut dire ajouter informations suplementaires; Les bean sont des classes
+						// suplementaires
 public class BiblioApplication {
 
 	public static void main(String[] args) {
