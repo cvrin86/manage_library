e400bbc8944e72fb0892889f4ad5361c126a0cb3
CristinaV Doranco <cristinavranceanu86@gmail.com>
2025-02-03 10:30:20 +0100

    Initializer project with Spring

---
+++ src/test/java/fr/doranco/biblio/BiblioApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.doranco.biblio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
